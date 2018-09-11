4c3b1c344ee511b834e8b39764733ed84b4a202d
Unknown <kennycheung388@yahoo.com.hk>
2018-09-11 15:41:30 -0700

    AddRecipePage

--- add-recipe.php
+++ add-recipe.php
@@ -3,8 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>title</title>
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
+    <link rel="stylesheet" href="css/css.css">
+    <!-- <script src="script.js"></script> -->
+
+    <div class="holder">
+      <h1>Hello World</h1>
+    </div>
   </head>
   <body>
     <!-- page content -->
