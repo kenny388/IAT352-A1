864cb7fc781e534f4b98899d491208bd34de691a
Unknown <kennycheung388@yahoo.com.hk>
2018-09-23 03:14:05 -0700

    Finished comenting all-recipes page

--- all-recipes.php
+++ all-recipes.php
@@ -9,12 +9,11 @@
     <title>All Recipes</title>
     <link rel="stylesheet" href="css/all-recipes.css">
     <link rel="stylesheet" href="css/headerFooter.css">
-    <!-- <script src="script.js"></script> -->
-
 
   </head>
   <body>
     <!-- page content -->
+    <!-- Included header with php -->
     <?php
     include 'header.php';
     ?>
@@ -26,39 +25,58 @@
 
       <hr>
 
+      <!-- This all-recipes page provide user a list of all recipes -->
+      <!-- Each contains basic informations such as name, description, prep&cook time and difficulty  -->
       <div class="container">
         <?php
+        //Loaded the file "kenny_cheung_recipes.txt"
         $file_lines = file('recipes/kenny_cheung_recipes.txt');
 
-        //Exception Handling if
+        //Exception Handling if there is no recipe
         if (sizeof($file_lines) <= 0) {
           echo '<p>No recipes available.</p>';
       }
+
+          //For loop looping through all the recipe lines available in the txt file
           foreach ($file_lines as $line) {
+            //using php explode function to separate each field with the symbol of ",_"
+            //Initially used ", " as the separation symbol, did not end well as the user-entered paragraphs are separated into many more segments
             $datas = explode(",_", $line);
+            //Actual form started, echoed most of the html parts as well
             echo '<div class="recipe">';
               echo '<div class="leftBox">';
                 echo '<div class="titleBox">';
+                  //When providing the link to "recipe-details.php",
+                  //I used an a tag with pointer towards recipe-details.php
+                  //And added the "?keyNum=" part with the 41st field in the data
+                  //the 41st field is where I provide the unique data identifier
+                  //With the the part "?keyNum=[identifier]", this will pass a $_GET field to the next page
+                  //Consequently providing the information of which data user clicked for the next page recipe-detail.php
                   echo '<a href="recipe-details.php?keyNum='; echo $datas[42]; echo'"><h3>' . $datas[0] . '</h3></a>';
                 echo '</div>';
                 echo '<div class="descriptionBox">';
+                  //$datas[3] is the short description field
                   echo '<p>' . $datas[3] . '</p>';
                 echo '</div>';
               echo '</div>';
               echo '<div class="rightBox">';
                 echo '<div class="prepCookBox">';
                   echo '<div class="innerLeftBox">';
+                    // $datas[4] and [5] are the preparation time value and unit
                     echo '<label>Prep: '. $datas[4] . $datas[5] .'</label>';
                   echo '</div>';
                   echo '<div class="innerRightBox">';
+                  // $datas[6] and [7] are the cook time value and unit
                   echo '<label>Cook: '. $datas[6] . $datas[7] .'</label>';
                 echo '</div>';
                 echo '</div>';
                 echo '<div class="servingsLevelBox">';
                   echo '<div class="innerLeftBox">';
+                    // $datas[8] is for servings field
                     echo '<label>Servings: '. $datas[8] .'</label>';
                   echo '</div>';
                   echo '<div class="innerRightBox">';
+                  // $datas[9] is for difficulty field
                   echo '<label>Level: '. $datas[9] .'</label>';
                 echo '</div>';
                 echo '</div>';
@@ -76,7 +94,7 @@
               <h3>NO NAME PIEEEEEE</h3>
             </div>
             <div class="descriptionBox">
-              <p>This is just sample text, Here is a pie, it has chicken, grass and everything except vegetables in it.</p>
+              <p>This is just sample text, Here is a pie, it has chicken, grass and everything except pineapple in it.</p>
             </div>
           </div>
           <div class="rightBox">
