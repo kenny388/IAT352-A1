c32e4d224a1bde21098aeb64eb3ce65fb3f4227b
Unknown <kennycheung388@yahoo.com.hk>
2018-09-23 03:47:27 -0700

    Success page commented

--- successful.php
+++ successful.php
@@ -8,38 +8,35 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Processing Entries</title>
+    <title>Entry Successful</title>
     <link rel="stylesheet" href="css/processingEntry.css">
     <link rel="stylesheet" href="css/headerFooter.css">
   </head>
   <body>
     <!-- page content -->
+    <!-- Included header with php -->
     <?php
     include 'header.php';
     ?>
+    <!-- Simple layout telling the users their entries are successful and provide the link to it -->
     <div class="holder">
       <h2 class="validatingText">Recipe Entry Successful!</h2>
       <hr>
       <p>Here is the link to your recipe page : </p>
+      <!-- Making the link with the size of recipes array - 1, this gives the newest recipe identifier-->
       <a href="<?php
         $file_lines = file('recipes/kenny_cheung_recipes.txt');
         $arraySize = sizeof($file_lines) - 1;
-
         echo 'recipe-details.php?keyNum=';
         echo $arraySize;
         echo '';
-
       ?>">Link</a>
     </div>
 
+    <!-- Included footer with php -->
     <?php
     include 'footer.php';
     ?>
-
-    <script type="text/javascript">
-        document.getElementById('formAuto').submit(); // SUBMIT FORM
-    </script>
-
 
   </body>
 </html>
